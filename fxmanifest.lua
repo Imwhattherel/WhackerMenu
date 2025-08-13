3add67e35dcce183c59e78e43541e87ca819f894
WhatTheReL <79932956+Imwhattherel@users.noreply.github.com>
2025-08-13 16:05:40 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'WhatTheReL'
-description 'Agency Menu to make setting codeplugs for SCNP25 simpler'
+description 'Agency Menu to make setting codeplugs for WhackerLink simpler'
 version '1.1'
 
 client_scripts {
